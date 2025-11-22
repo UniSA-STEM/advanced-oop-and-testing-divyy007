af3059c606703984f13ae03cc9adae3a6f6b5b67
divyy007 <divyy007@mymail.unisa.edu.au>
2025-11-23 03:44:47 +0530

    Add Staff class with initialization and string representation methods

--- staff.py
+++ staff.py
@@ -6,3 +6,19 @@ ID: 110449639
 Username: divyy007
 This is my own work as defined by the University's Academic Integrity Policy.
 '''
+
+from animal import Animal
+from enclosure import Enclosure
+
+class Staff:
+    """Base class for all staff members."""
+    def __init__(self, name: str, employee_id: str):
+        # Private attributes
+        self.__name = name
+        self.__employee_id = employee_id
+        
+    def get_name(self):
+        return self.__name
+
+    def __str__(self):
+        return f"Staff: {self.__name} (ID: {self.__employee_id})"
