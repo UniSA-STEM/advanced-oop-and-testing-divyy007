c6025bf4ff87739dccd0ca5e8c641d2030879cbb
divyy007 <divyy007@mymail.unisa.edu.au>
2025-11-23 03:51:32 +0530

    Add Zookeeper class with feeding and cleaning methods for animal care

--- staff.py
+++ staff.py
@@ -22,3 +22,21 @@ class Staff:
 
     def __str__(self):
         return f"Staff: {self.__name} (ID: {self.__employee_id})"
+    
+class Zookeeper(Staff):
+    """Staff specialized in daily care and enclosure maintenance (Specialisation) [34]."""
+    def __init__(self, name: str, employee_id: str):
+        super().__init__(name, employee_id)
+        
+    def feed_animal(self, animal: Animal):
+        """Zookeeper performs the feeding action (Association) [33]."""
+        # Invokes the polymorphic method on the animal object
+        action = animal.eat()
+        return f"{self.get_name()} is feeding {animal.get_name()}. Result: {action}"
+        
+    def clean_enclosure(self, enclosure: Enclosure):
+        """Zookeeper performs cleaning duties."""
+        # Calls the public method on the enclosure object
+        enclosure.clean() 
+        return f"{self.get_name()} cleaned {enclosure.get_name()}. Cleanliness is now maxed out."
+
